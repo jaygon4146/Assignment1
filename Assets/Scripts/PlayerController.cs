7f0e974bd93a92d14a4078f35dd9e06c5c1d91ff
jaygon4146 <jaygon4146@gmail.com>
2015-03-11 15:05:05 +1100

    First Commit - test
    Signed-off-by: jaygon4146 <jaygon4146@gmail.com>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Get Input
+
+	//Apply Input
+
+	public Rigidbody rb;
+	public float speed;
+	private int count;
+
+	void Start() {		
+		rb = GetComponent <Rigidbody> ();
+		count = 0;
+	}
+
+	void Update ()		//Called before rendereing a frame
+	{
+	}
+
+	void FixedUpdate ()	//Called before applying physics
+	{
+		float moveHorz = Input.GetAxis ("Horizontal");
+		float moveVert = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorz, 0.0f, moveVert);
+
+		//rigidbody.AddForce (movement);
+		rb.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Destroy (other.gameObject);
+		if (other.gameObject.tag == "Pickup") 
+		{
+			other.gameObject.SetActive (false);
+			count += 1;
+			
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
